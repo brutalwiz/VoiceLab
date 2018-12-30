7558c3a220d4e79147854f4e84f67b7a294d3a03
brutalwiz <janlukasfeldmann@gmail.com>
2018-12-30 16:09:19 +0100

    Updated Freezing

--- Arduino Project/TeensyScript/TeensyScript.ino
+++ Arduino Project/TeensyScript/TeensyScript.ino
@@ -2,6 +2,12 @@
 // then in tools > USB Type: select "MIDI"
 // the MIDI channel number to send messages
 
+//Setup Button
+int bankTaste = LOW;
+int bankTasteAlt = LOW;
+int bankTastePin = 2;
+bool freeze = false;
+//Setup FSR
 int previousA0 = -1; // store previously sent values, to detect changes
 int previousA1 = -1;
 int previousA2 = -1;
@@ -16,25 +22,49 @@ int n2;
 int n3;
 
 void setup() {  
+  pinMode(bankTastePin, INPUT_PULLUP);
   Serial.begin(96000);
 
 }
 
 void loop() {
-  n0 = analogRead(druckKnopfZeige)/8; //convert 10bit analog signal to 7 bit MIDI
-  n1 = analogRead(druckKnopfMittel)/8;
-  n2 = analogRead(druckKnopfRing)/8;
-  n3 = analogRead(druckKnopfKlein)/8;
-  // only transmit MIDI messages if analog input changed
-  sPrint(n0);
-  sPrint(n1);
-  //sPrint(n2); REENABLE LATER
-  //sPrint(n3);  ...
+  bankTaste = digitalRead(bankTastePin);
+  //Button pressed
+  if(bankTaste==LOW && bankTasteAlt == HIGH){
+
+    if(!freeze){
+      freeze=true;
+    }else{
+      freeze=false;
+    }
+
+    bankTasteAlt=bankTaste;
+  }
+
+  //Button Released
+  if(bankTaste==HIGH && bankTasteAlt == LOW){
+
+    //...do stuff
+
+    bankTasteAlt=bankTaste;
+  }
+
+  if(!freeze){
+    n0 = analogRead(druckKnopfZeige)/8; //convert 10bit analog signal to 7 bit MIDI
+    n1 = analogRead(druckKnopfMittel)/8;
+    n2 = analogRead(druckKnopfRing)/8;
+    n3 = analogRead(druckKnopfKlein)/8;
+    // only transmit MIDI messages if analog input changed
+    sPrint(n0);
+    sPrint(n1);
+    //sPrint(n2); REENABLE LATER
+    //sPrint(n3);  ...
   
-  sendMidiData(20,n0,previousA0);
-  //sendMidiData(21,n1,previousA1); REENABLE LATER
-  //sendMidiData(22,n2,previousA2); ..
-  //sendMidiData(23,n3,previousA3); ..
+    sendMidiData(20,n0,previousA0);
+    //sendMidiData(21,n1,previousA1); REENABLE LATER
+    //sendMidiData(22,n2,previousA2); ..
+    //sendMidiData(23,n3,previousA3); ..
+  }
 
   // MIDI Controllers should discard incoming MIDI messages.
   // http://forum.pjrc.com/threads/24179-Teensy-3-Ableton-Analog-CC-causes-midi-crash
